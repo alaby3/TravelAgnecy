57a7aa71ae8832f58675a7be1d9962715d9bddf3
ryba0nat <119972252+ryba0nat@users.noreply.github.com>
2026-04-15 21:32:37 +0200

    Dodanie klasy Trip

---
+++ DDD.TravelAgency.Core/DomainModelLayer/Models/Trip.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.TravelAgency.Core.DomainModelLayer.Models
+{
+
+
+    //public enum 
+    public  class Trip
+    {
+
+        public long Id { get; set; } 
+        public string Name { get; set; }    
+        public string Description { get; set; }
+        
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+}
